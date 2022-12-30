d3d7327cc3d47a5ba8232fa0683114aa8781c04a
Ilya <97946899+Ilya-Polyanichko@users.noreply.github.com>
2022-12-31 00:52:48 +0300

    2.0

--- Collection/Task I/Program.cs
+++ Collection/Task I/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int unitSum(int n) => (n * (n + 1)) / 2;
+
+int sum = 3 * unitSum((int)(999 / 3)) + 5 * unitSum((int)(999 / 5)) - 15 * unitSum((int)(999 / 15));
+Console.WriteLine($"Сумма всех кратных 3 и 5 ниже 1000 равна {sum}");
---
+++ Collection/Task II/Program.cs
@@ -0,0 +1,10 @@
+﻿int Fib(int n)
+{
+    double sq5 = Math.Sqrt(5);
+    double a = (1 + sq5) / 2;
+    double b = (1 - sq5) / 2;
+
+    return (int)((Math.Pow(a, n) - Math.Pow(b, n)) / sq5);
+}
+
+Console.WriteLine(Fib(7));
